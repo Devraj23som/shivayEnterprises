a1ed9e5ebc33a4af87b2641e7a24aafe62503bcb
Devraj23som <96883432+Devraj23som@users.noreply.github.com>
2026-05-05 16:51:20 +0530

    Update Home.jsx
    num.changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,8 +16,8 @@ const Home = () => {
 सेंटर</h3>
      </div>
      <div className=' w-7/10 p-2 mx:auto '>
-     <a href={`tel:${'+919109298201'}`}>
-       <h3 className='rounded-2xl text-2xl text-center bg-orange-500 text-white'>Call Now <br />+91 9109298201</h3>
+     <a href={`tel:${'+919039470978'}`}>
+       <h3 className='rounded-2xl text-2xl text-center bg-orange-500 text-white'>Call Now <br />+91 9039470978</h3>
       </a>
      </div>
     
